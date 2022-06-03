bcae8a2da8d2197b923306ae44fc5b55db57653e
ramiinberg <rami.inberg@gmail.com>
2022-06-03 10:08:19 +0300

    changed fine print text

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ emailCollectorForm.addEventListener("submit", event => {
   const ourFormData = new FormData(event.target)
 
   const userFirstName = ourFormData.get("firstName")
+  const userEmail = ourFormData.get("emailAddress")
 
   const updatedHtmlContent = `
   <h2>Congratulations, ${userFirstName}</h2>
 
   <p class="subtitle"> You're on your way to becoming a BBQ Master! </p>
 
-  <p class="fine-print">We'll never share your information without your permission</p>
+  <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmail}</p>
   `
 
   mainContent.innerHTML = updatedHtmlContent
